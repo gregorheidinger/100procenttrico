29958b947299ab9d8c00bd0d58d87dcea19b1e08
gregorheidinger <gregor@heidinger.nl>
2026-08-30 20:47:53 +0200

    Expand Christmas show options

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ const maatwerkPhoto=document.querySelector('#maatwerk .widephoto img');
 if(maatwerkPhoto){maatwerkPhoto.src='assets/images/originals/002.png';maatwerkPhoto.alt='100% Trico live';}
 const galleryMiddle=document.querySelector('.gallery img:nth-child(2)');
 if(galleryMiddle){galleryMiddle.src='assets/images/originals/010.jpg';galleryMiddle.alt='100% Trico live';}
+const kerst=document.querySelector('#kerst .christmas-copy');
+if(kerst){
+  const title=kerst.querySelector('h2');
+  const bigcopy=kerst.querySelector('.bigcopy');
+  const body=bigcopy?.nextElementSibling;
+  if(title) title.textContent='Geen stille nacht.';
+  if(bigcopy) bigcopy.textContent='Kerst met 100% Trico is warm, uitbundig en vooral live.';
+  if(body) body.innerHTML='<strong>KERSTSHOW</strong><br>Een feestelijk optreden vol bekende kersthits, mooie samenzang, humor en contact met het publiek. Geschikt als zelfstandig optreden, tijdens een diner, of als onderdeel van een kerstfeest of kerstborrel.<br><br><strong>KERST MUZIEKBINGO</strong><br>Bingo, maar dan helemaal in kerstsfeer. Herken o.a. de kersthits, kruis ze af op je bingokaart en zing vooral uit volle borst mee. Natuurlijk zijn er ook prijzen te winnen.<br><br><strong>KERSTPAKKETTEN UITDELEN</strong><br>Maak van het uitdelen van de kerstpakketten een feestje. Combineer het bijvoorbeeld met de Kerst Muziekbingo, waarbij uiteindelijk iedereen tegelijk bingo heeft. En wat is de prijs? Natuurlijk: het kerstpakket! Zo wordt het uitdelen zelf onderdeel van het entertainment.<br><br><strong>IETS BIJZONDERS IN GEDACHTEN?</strong><br>Een medewerker in het zonnetje zetten, een kerstlied op maat of verschillende onderdelen combineren? We maken de kerstshow graag passend bij jullie feest.';
+}
